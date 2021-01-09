0d3dc852e4ed60e78694b510ff909d3b74c2462a
Ulrich Lichtenegger <ulilicht@googlemail.com>
2021-01-09 19:42:01 +0100

    Fix https://github.com/ulilicht/Raumbar/issues/10

--- electron/main.js
+++ electron/main.js
@@ -8,13 +8,16 @@ let mb;
 const startUrl = process.env.ELECTRON_START_URL || `file://${path.join(__dirname, "../build/index.html")}`;
 const isDevelopmentMode = process.env.RAUMBAR_DEVELOPMENT_MODE === "true";
 
-
 function createMenubar() {
     const iconPath = path.join(__dirname, '..', 'public', 'speaker-icon@2x.png');
 
     const tray = new Tray(iconPath);
     const contextMenu = Menu.buildFromTemplate([
-        {label: 'Report issue', type: 'normal', click: () => {shell.openExternal('https://github.com/ulilicht/Raumbar/issues')}},
+        {
+            label: 'Report issue', type: 'normal', click: () => {
+                shell.openExternal('https://github.com/ulilicht/Raumbar/issues')
+            }
+        },
         {label: 'Quit Raumbar', type: 'radio', role: 'quit'},
     ])
 
@@ -25,13 +28,23 @@ function createMenubar() {
             webPreferences: {
                 preload: path.join(__dirname, 'preload.js'),
             },
-            vibrancy: 'light'
+            vibrancy: 'light',
+            visualEffectState: "active",
+            alwaysOnTop: true
         }
     });
 
     tray.on('right-click', function (event) {
         tray.popUpContextMenu(contextMenu);
     });
+
+    mb.on('show', () => {
+        if (mb.window) {
+            mb.window.setOpacity(1); // reset the opacity which was set with the fade-out effect.
+        }
+    });
+
+    mb.on('focus-lost', () => fadeOut(mb));
 }
 
 app.on('ready', createMenubar);
@@ -42,4 +55,25 @@ if (!isDevelopmentMode) {
         openAtLogin: true,
         openAsHidden: true
     });
+}
+
+
+function fadeOut(mb) {
+    const step = 0.1;
+    const fadeEveryXSeconds = 15;
+    if (!mb.window) {
+        return;
+    }
+
+    let opacity = mb.window.getOpacity();
+
+    const interval = setInterval(() => {
+        mb.window.setOpacity(opacity);
+        opacity -= step;
+
+        if (opacity <= 0.2) {
+            clearInterval(interval);
+            mb.hideWindow();
+        }
+    }, fadeEveryXSeconds);
 }
